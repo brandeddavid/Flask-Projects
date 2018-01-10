8259d40fb34e3182ba0433f57672f7d8a70931ab
DavidMwangi <david.mathenge98@gmail.com>
2018-01-10 16:56:02 +0300

    Add tojSon test file

---
+++ d3.js/tojson.py
@@ -0,0 +1,23 @@
+import csv, json
+
+jsonObject = {"POI": 201, 'calldetails':[]}
+
+with open("210.csv", "r") as file:
+
+	reader = csv.reader(file, delimiter=',')
+
+	details = {}
+
+	for item in reader:
+
+		details['phoneNumber'] = item[1]
+		details["callType"] = item[4]
+
+		jsonObject["calldetails"].append(details)
+
+		details = {}
+
+	jsonObject = json.dumps(jsonObject)
+
+print (jsonObject)
+
